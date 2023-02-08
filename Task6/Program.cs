a5593660cb1ce7d3e4b3a65a7d44ee77a8152f4b
Ushiyariv.Timur <uti2000@yandex.ru>
2023-02-09 00:53:41 +0300

    Added sixth task

---
+++ Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿// 6. Показать кубы чисел, заканчивающихся на четную цифру;
+
+System.Console.WriteLine();
+
+int N = new Random().Next(1, 30);
+System.Console.WriteLine($"N = {N}");
+
+int number = 1;
+
+while (number <= N)
+{
+    int cube = number * number * number;
+    if (cube % 2 == 0) System.Console.Write(cube + " ");
+    number++;
+}
+System.Console.WriteLine();
